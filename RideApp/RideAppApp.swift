be11f97e45e4adef132d2b281bfa9e31d735707a
aLLUPS <73pawara@gmail.com>
2023-01-11 19:45:33 +0530

    Initial Commit

---
+++ RideApp/RideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideAppApp.swift
+//  RideApp
+//
+//  Created by Pawara Siriwardhane on 2023-01-11.
+//
+
+import SwiftUI
+
+@main
+struct RideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
